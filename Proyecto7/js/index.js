8da7d03eebaef212f4518bf71fe61114d20d2b5a
Abraham Rodriguez <abrahamrodricontr@gmail.com>
2024-03-21 18:16:08 -0600

    Calculadora terminada

--- Proyecto7/js/index.js
+++ Proyecto7/js/index.js
@@ -1,6 +1,6 @@
 let resultado = document.getElementById('Resulatdo');
 
-let asignar = (valor) =>{
+function asignar(valor){
     resultado.value += valor;
 }
 
@@ -11,3 +11,22 @@ let limpiar =() => {
 let calcular = () => {
     resultado.val = eval(resultado.value);
 }
+
+let SquareRoot = () => {
+    resultado.value = Math.sqrt(resultado.value);
+}
+
+let porcentaje = () => {
+    resultado.value = resultado.value / 100;
+}
+
+let cuadrado = () => {
+    resultado.value = resultado.value * resultado.value;
+}
+
+let Coseno = () => {
+    resultado.value = Math.cos(resultado.value);
+}
+
+
+
