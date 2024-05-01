b96b8b04ed4a4e7c252be06bb3dd1e1476495764
Abraham Rodriguez <abrahamrodricontr@gmail.com>
2024-04-30 19:49:54 -0600

    agregue mas cosas

--- Practica8/js/JavaScrip.js
+++ Practica8/js/JavaScrip.js
@@ -4,9 +4,35 @@
     let descripcion = document.getElementById('descripcion');
 
     let tareas = [];
-
+    let listaTareas = document.getElementById('listaTareas');
     let btnGuardar = document.getElementById('btnGuardar');
 
+    function mostrarTareas(){
+        listaTareas.innerHTML = "Esta es mi lista de tareas";
+        tareas.forEach((tarea,indice) => {
+            listaTareas.innerHTML += `
+            <div class='row'>
+                <div class='col-3 border p-3'>
+                    <strong>${ tarea.nombre }</strong>
+                </div>
+                <div class='col-3 border p-3'>
+                    <strong>${ tarea.fecha }</strong>
+                </div>
+                <div class='col-4 border p-3'>
+                    <strong>${ tarea.descripcion }</strong>
+                </div>
+                <div class='col-1 border p-3 text-center'>
+                    <button class='btn btn-success'>Editar</button>
+                </div>
+                <div class='col-1 border p-3 text-center'>
+                    <button class='btn btn-danger'>Borrar</button>
+                </div>
+            </div>
+            `;
+        });
+    }
+
+mostrarTareas();
     let agregarDatos = function() {
         let datos = {
             nombre: nombre.value,
@@ -15,6 +41,7 @@
         }
         tareas.push(datos);
         console.log(tareas);
+        mostrarTareas();    
     }
 
     formulario.addEventListener('submit', (e) => { //Evento submit
