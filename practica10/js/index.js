283a2b461f575f6c509effc06208c61001b7a9d2
80259 - Abraham Rodríguez Contreras <arc80259@lasallebajio.edu.mx>
2024-05-21 23:44:12 -0600

    el de la tienda

---
+++ practica10/js/index.js
@@ -0,0 +1,33 @@
+let listaProductos = document.querySelector("#listaProductos");
+
+function obtieneProductos(){
+    //promesas
+    fetch('https://fakestoreapi.com/products/')
+                .then(res=>res.json())
+                .then(productosObtenidos=>{
+                    console.log("Productos obtenidos " + productosObtenidos);
+    
+                    productos = productosObtenidos.slice();
+    
+                    productosObtenidos.forEach((producto, indice) => {
+                        console.log("producto " + producto);
+    
+                        listaProductos.innerHTML += `
+                        <div class="col-12 col-md-3">
+                        <div class="card" >
+                        <img src="${producto.image }" class="imagenProducto card-img-top py-2" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${producto.title}</h5>
+                            <p class="card-text"> ${producto.description.slice(0,100)}</p>
+                            <p class="text-danger"> ${producto.price}</p>
+                            <a href="#" class="btn btn-primary">Comprar</a>
+                        </div>
+                        </div>
+                        </div>
+                        
+                        `              });
+                });
+    }
+    
+    obtieneProductos();
+    
